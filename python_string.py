a602b4fa13ac710b00107cb44b63f2866a78e161
vinay <vinaykumargattu@gmail.com>
2020-04-04 20:05:11 +0530

    Variable program added

---
+++ python_string.py
@@ -0,0 +1,18 @@
+name=input("Enter your name")
+test1=name.encode()
+print(test1)
+x=len(name)
+print(x)
+for i in range(0, len(name)):
+               print(name[i])
+abc=len(name)
+print(abc)
+uppercare = name.upper()
+contitle=name.title()
+l1=name.swapcase()
+
+print(l1)
+print(contitle)
+print(uppercare)
+
+
