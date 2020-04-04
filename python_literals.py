2a9dd6b84a6883accacaf1fd4e41068756cc1dd3
vinay <vinaykumargattu@gmail.com>
2020-04-04 14:21:12 +0530

    Variable program added

---
+++ python_literals.py
@@ -0,0 +1,21 @@
+# We have different kinds of string literals
+
+name1='vinay'
+
+print(name1)
+
+name2="vinay kumar"
+
+print(name2)
+
+
+name3='hello\
+      vinay kumar'
+
+print(name3)
+
+name4=""" vinay
+Kumar 
+is great """
+
+print(name4)
