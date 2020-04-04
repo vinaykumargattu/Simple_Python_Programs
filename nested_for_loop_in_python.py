ea641c6d8423434ae83d2e2a93f76362d6442984
vinay <vinaykumargattu@gmail.com>
2020-04-04 18:50:37 +0530

    Variable program added

--- nested_for_loop_in_python.py
+++ nested_for_loop_in_python.py
@@ -7,8 +7,19 @@ for i in range(0,x):
     for j in range(0,i+1):
         print("*", end='')"""
 
-number1=int(input("Enter the number"))
+#for loop with else
+
+"""number1=int(input("Enter the number"))
 for i in range(0,number1):
     print(i)
 else:
     print("Forloop completed")
+"""
+    #for loop with break statement
+
+number2=int(input("Enter the number"))
+for i in range(0, number2):
+    print(i)
+    break;
+else:
+    print("for loop once executed and  exit due to break condition")
---
+++ python_while_loop.py
@@ -0,0 +1,23 @@
+#Python while loop
+
+#while loop using if condition
+
+"""number1=int(input("Enter a number"))
+while number1<=10:
+    print(number1)
+    number1 = number1 + 1
+    if(number1==7):
+        print("The for loop reches to %d"%number1)
+"""
+
+#while loop using break conditon
+
+number2=int(input("Enter a number"))
+while number2<=10:
+    print(number2)
+    number2=number2+1
+    if (number2==6):
+        print("The loop break at %d"%number2)
+        break;
+
+
