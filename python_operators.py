88864d5498da64acee514deb3c9db0e63294cad6
vinay <vinaykumargattu@gmail.com>
2020-04-04 14:56:02 +0530

    Variable program added

---
+++ python_operators.py
@@ -0,0 +1,36 @@
+#Python Operators
+
+#Arithmetic operators
+
+x=int(input("Enter x value"))
+y=int(input("Enter y value"))
+
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+e=x%y
+f=x**y
+g=x//y
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+
+#Comparison operator
+h=x==y
+i=x!=y
+j=x<=y
+k=x>=y
+l=x<y
+m=x>y
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
