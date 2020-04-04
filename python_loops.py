8633102759b08ac4931415f685d0dd872adcdf60
vinay <vinaykumargattu@gmail.com>
2020-04-04 18:20:20 +0530

    Variable program added

--- python_loops.py
+++ python_loops.py
@@ -1,5 +1,11 @@
-#simple mutiplication table program 
+#Loops simple mutiplication table program
 
 i=int(input("Enter the table number"))
 for x in range(1,11):
     print("%d x %d=%d"%(i,x,i*x))
+
+
+"""j=int(input("Enter a number"))
+for x in range(0,10):
+    print(x, end=' ')
+"""
