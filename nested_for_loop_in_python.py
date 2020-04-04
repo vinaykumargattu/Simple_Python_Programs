e20885a71d1bc3ccb0fa082f4990a2a70441ac33
vinay <vinaykumargattu@gmail.com>
2020-04-04 18:28:54 +0530

    Variable program added

---
+++ nested_for_loop_in_python.py
@@ -0,0 +1,14 @@
+#Nested for loop in python
+
+"""x=int(input("Enter a number"))
+i,j=0,0
+for i in range(0,x):
+    print("")
+    for j in range(0,i+1):
+        print("*", end='')"""
+
+number1=int(input("Enter the number"))
+for i in range(0,number1):
+    print(i)
+else:
+    print("Forloop completed")
