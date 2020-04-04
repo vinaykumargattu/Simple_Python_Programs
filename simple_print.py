4f9231f42ddf59236ab9f7e27a1d046dc26af72f
vinay <vinaykumargattu@gmail.com>
2020-04-04 12:42:06 +0530

    intial commit

---
+++ simple_print.py
@@ -0,0 +1,19 @@
+#Simple printing informatoin using print command
+
+print("Hello welcome to python programming")
+
+#simpel variable declaration
+
+a = 10
+
+print(a, type(a))
+
+name="vinaykumar"
+
+print(name)
+
+#to know the variable type
+
+print(type(name))
+
+print("hello")
