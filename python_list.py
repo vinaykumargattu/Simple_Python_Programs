a904556e6db7ca217c8de5a4e58583265b5fb220
vinay <vinaykumargattu@gmail.com>
2020-04-05 22:52:32 +0530

    Variable program added

---
+++ python_list.py
@@ -0,0 +1,24 @@
+#Python List
+
+"""list = ["a", "b", "c", "d", "e"]
+list1 = ["f", "g", "h"]
+list2=list+list1;
+for i in list2:
+    print(i)
+   """
+list = ["shiva", "ram", "krishna"]
+list1 = [101, "shiva", 12]
+
+print("id is %d, Name is %s, Complex Number is %d "%(list1[0], list1[1], list1[2] ))
+list1.append("kumar")
+list1[0]
+list1[1]
+list1[2]
+list1[3]
+print(list1[3])
+print(list1)
+print(type(list1[0]))
+print(type(list1[1]))
+print(type(list1[2]))
+print(type(list[0]))
+
