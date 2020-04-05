d1f9df82b8d70eb84435057c310ac44657de7dbd
vinay <vinaykumargattu@gmail.com>
2020-04-05 23:27:07 +0530

    Variable program added

---
+++ python_ tuple.py
@@ -0,0 +1,12 @@
+#Python Tuple
+t2=(101, "vinaykumar", 33)
+print(t2)
+
+
+
+
+t1=[(101, "vinay"), (102, "kumar"), (103, "shiva")]
+for i in t1:
+    print(i)
+    t1[0]=(105, "priya")
+    print(t1)
