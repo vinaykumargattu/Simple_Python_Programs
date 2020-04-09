08ef517799aad1043eeb2c7294fae650bceb781f
vinay <vinaykumargattu@gmail.com>
2020-04-09 09:34:39 +0530

    Program Updated

--- python_modules.py
+++ python_modules.py
@@ -1,1 +1,18 @@
-print("Om Namhashivaya")
+import json
+from math import factorial
+
+class Mymath():
+    def __init__(self):
+        a=5
+        factval =factorial(a)
+        print(type(factval))
+        print(factval)
+        print(factorial(a))
+
+
+my = Mymath()
+
+#The dir() function returns a sorted list of names defined in the passed module. 
+
+list = dir(json)
+print(list)
